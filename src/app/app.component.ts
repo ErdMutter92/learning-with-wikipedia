51fb26d688594f5b3a44e7548fd5ec0256e653de
Brandon Bleau <bmbleau@gmail.com>
2024-03-30 02:12:08 -0400

    move tutorial to translation file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ import { TranslateDirective } from './language/translate.directive';
 import { LanguageSettings } from './language/language.model';
 import { SET_LANGUAGE } from './language/language.actions';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { selectedLanguage } from './language/language.selectors';
+import { selectedLanguage, translation } from './language/language.selectors';
 import introJs from 'intro.js';
 
 function noWhitespaceValidator(control: FormControl) {
@@ -59,7 +59,8 @@ export class AppComponent {
 
   public readonly loading$ = this.articleStore.pipe(select(loading));
 
-  public readonly language$ = this.languageSettingsStore.pipe(select(selectedLanguage));
+  public readonly selectedLanguage$ = this.languageSettingsStore.pipe(select(selectedLanguage));
+  public readonly translations = this.languageSettingsStore.pipe(select(translation));
 
   public readonly articleId$ = this.articleStore.pipe(select(selectedId));
   public readonly title$ = this.articleStore.pipe(select(articleTitle));
@@ -93,48 +94,20 @@ export class AppComponent {
   ) {}
 
   public ngAfterViewInit() {
-    setTimeout(() => {
-      introJs().setOptions({
-        tooltipClass: 'eTutor',
-        dontShowAgain: true,
-        dontShowAgainCookie: 'tutor-v1-bwn-wiki-learning',
-        steps: [
-          {
-            title: 'Welcome to Wiki Learning by BWN',
-            intro: 'Allow me to introduce you to Wiki Learning from Bleau Web Network. A language learning tool for practicing vocabulary and discovering words! The aim of the game is to guess all the words needed to unmask the entire article.'
-          },
-          {
-            element: '[data-intro-id="language-selector"]',
-            title: 'Choose your language',
-            intro: 'We support multiple languages. You are welcome to switch between and use as many of them as you wish. Your progress is saved as you go.'
-          },
-          {
-            element: '[data-intro-id="lesson-card"]',
-            title: 'Your current article',
-            intro: 'Here is your active article. For each word you guess correctly they will appear before your eyes here. If you guesses are close enough the colors will change to provide hints to what is underneath.'
-          },
-          {
-            element: '[data-intro-id="guess-input"]',
-            title: 'Input your guesses',
-            intro: 'Type your guesses into this box. Upon pressing enter your guess will be checked with all the words in the article and added to your guess list.'
-          },
-          {
-            element: '[data-intro-id="right-drawer"]',
-            title: 'Past guesses',
-            intro: 'You can track what you have entered into the text box here.'
-          },
-          {
-            element: '[data-intro-id="buy-me-a-coffee"]',
-            title: 'Please consider donating',
-            intro: 'Sometime in the future, if you are able to and have gotten some good use out of this app, please consider donating. This will help me bring more educational resources to people for free.'
-          }
-        ]
-      }).start();
-    }, 100);
+    this.translations.pipe(map(({ tutorial }) => tutorial), take(1)).subscribe((steps) => {
+      setTimeout(() => {
+        introJs().setOptions({
+          tooltipClass: 'eTutor',
+          dontShowAgain: true,
+          dontShowAgainCookie: 'tutor-v1-bwn-wiki-learning',
+          steps
+        }).start();
+      }, 100);
+    });
   }
 
   public ngOnInit() {
-    this.language$.pipe(take(1)).subscribe((language: any) => {
+    this.selectedLanguage$.pipe(take(1)).subscribe((language: any) => {
       if (language) this.languageSelector.setValue(language);
     });
     
