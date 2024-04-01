d5ff3cd30174d01d1e17a17d3d82aea5ee18d82e
Brandon Bleau <bmbleau@gmail.com>
2024-04-01 02:33:43 -0400

    readd unmask and reset article

--- src/app/views/reader/reader.page.ts
+++ src/app/views/reader/reader.page.ts
@@ -5,7 +5,7 @@ import { article } from "../../state/library/article.selectors";
 import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { MatInputModule } from "@angular/material/input";
-import { ADD_GUESS } from "../../state/library/article.actions";
+import { ADD_GUESS, ARTICLE_UNMASK, RESET_ARTICLE } from "../../state/library/article.actions";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { ArticleMaskDirective } from "../../state/library/article-mask.directive";
@@ -46,9 +46,17 @@ export class ReaderPage {
         }
     }
 
-    public reset(articleId?: string): void {}
+    public reset(articleId?: string): void {
+        if (articleId) {
+            this.articleStore.dispatch(RESET_ARTICLE({ id: articleId }));
+        }
+    }
 
-    public unmask(articleId?: string): void {}
+    public unmask(articleId?: string): void {
+        if (articleId) {
+            this.articleStore.dispatch(ARTICLE_UNMASK({ id: articleId }));
+        }
+    }
 
     public preventInput(key: string) {
         return (event: KeyboardEvent) => {
